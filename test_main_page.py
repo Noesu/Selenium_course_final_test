40b78e75b68239aed30b03e4ac871edc5201fc02
Noesu <noesugar@bk.ru>
2024-09-27 16:49:29 +0300

    login_page tests added

--- test_main_page.py
+++ test_main_page.py
@@ -1,13 +1,6 @@
-from selenium.webdriver.common.by import By
-
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
 
-
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -16,5 +9,5 @@ def test_guest_should_see_login_link(browser):
     page.should_be_login_link()
 
 def go_to_login_page(browser):
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
